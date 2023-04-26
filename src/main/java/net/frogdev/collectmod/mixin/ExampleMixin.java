3f0608a7f70ac01535389052393835e46a0324ff
Ewan Sharp <ewan.sharp.dev@gmail.com>
2023-04-27 00:45:06 +0100

    what

---
+++ src/main/java/net/frogdev/collectmod/mixin/ExampleMixin.java
@@ -0,0 +1,18 @@
+package net.frogdev.collectmod.mixin;
+
+
+import net.frogdev.collectmod.CollectMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+
+		CollectMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
